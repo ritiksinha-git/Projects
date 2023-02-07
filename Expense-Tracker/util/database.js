6f892a140cda444faf9d4b662d0e8f98c09d406f
ritik sinha <ritikxsinha@gmail.com>
2023-02-07 15:43:02 +0530

    moved frontend

--- Expense-Tracker/util/database.js
+++ Expense-Tracker/util/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('et', 'root', '12345678', {
+const sequelize = new Sequelize('expense-tracker', 'root', '12345678', {
     dialect: 'mysql',
     host: 'localhost'
     
