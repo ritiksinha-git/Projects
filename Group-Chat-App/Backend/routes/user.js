aa013f4ff3bf48ce801d6f6d3dbb471b027bdd83
ritik sinha <ritikxsinha@gmail.com>
2023-02-20 14:53:28 +0530

    Final Touches

--- Group-Chat-App/Backend/routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-
-// POST route for user sign up
-router.post('/signup', userController.signup);
-router.post('/login', userController.login);
-
-module.exports = router;
