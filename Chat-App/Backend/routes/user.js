f7f3e57030b458c289f2df6339cc585381c98970
ritik sinha <110479671+ritiksinha-git@users.noreply.github.com>
2025-09-17 11:53:03 +0530

    Delete Chat-App directory

--- Chat-App/Backend/routes/user.js
+++
@@ -1,7 +0,0 @@
-const express=require('express');
-const router=express.Router();
-const signupController=require('../controllers/signup')
-
-router.post('/signup',signupController.signup);
-router.post('/login',signupController.login);
-module.exports = router;
